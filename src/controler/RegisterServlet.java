7a63dca0287641cdfa3a3251a24f292be72e7f56
Ryu <neverbackdown12345@abv.bg>
2017-10-16 17:39:58 +0300

    new jsp

--- src/controler/RegisterServlet.java
+++ src/controler/RegisterServlet.java
@@ -1,11 +1,15 @@
 package controler;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.db.UserDao;
 
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
@@ -21,6 +25,14 @@ public class RegisterServlet extends HttpServlet {
 		String email = request.getParameter("email");
 		String phone = request.getParameter("phone");
 		String address = request.getParameter("address");
+
+		try {
+			if (UserDao.getInstance().userExistsByEmail(email)) {
+				response.sendRedirect("existsuser.jsp");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
