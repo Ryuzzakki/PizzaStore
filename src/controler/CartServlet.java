00151de57608461698c01e1c56ee58c314dd16f8
Ryu <neverbackdown12345@abv.bg>
2017-10-19 19:55:06 +0300

    fix on cart

--- src/controler/CartServlet.java
+++ src/controler/CartServlet.java
@@ -28,29 +28,27 @@ public class CartServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("invoked 1");
+
 		User currUser = (User) request.getSession().getAttribute("user");
 		Restaurant currRestaurant = (Restaurant) request.getSession().getAttribute("restaurant");
 
 		if (request.getSession().getAttribute("order") == null) {
 			try {
-				System.out.println("invoked2 ");
+
 				long orderId = OrderDao.getInstance().createOrder(currUser, currRestaurant);
 				Order order = OrderDao.getInstance().getOrderById(orderId);
 				request.getSession().setAttribute("order", order);
-				System.out.println("hi");
+
 			} catch (UserException | SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		String id = request.getParameter("productId");
-		System.out.println("invoked 3");
 		try {
 			Product p = ProductDao.getInstance().getProduct(Long.parseLong(id));
 			Order order = (Order) request.getSession().getAttribute("order");
 			OrderDetailsDao.getInstance().addProductToOrderDetails(p, order, 1);
-			System.out.println("invoked 4");
-			OrderDao.getInstance().updateOrderPrice(p.getPrice(), order.getId());
+			OrderDao.getInstance().calculatePrice(order.getId());
 			// probably transaction
 			// fix restaurant setting
 		} catch (NumberFormatException | SQLException e) {
@@ -63,6 +61,10 @@ public class CartServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		if (req.getSession().getAttribute("order") == null) {
 			req.getRequestDispatcher("main.jsp").forward(req, resp);
+			return;
+		}
+		if (req.getSession().getAttribute("user") == null) {
+			req.getRequestDispatcher("login.jsp").forward(req, resp);
 			return;
 		}
 		Order order = (Order) req.getSession().getAttribute("order");
