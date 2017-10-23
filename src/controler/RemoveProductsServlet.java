2be28a7daa1f60a8deec4baf9cc236350c9b0bcd
Ryu <neverbackdown12345@abv.bg>
2017-10-23 23:34:33 +0300

    fix on the fix

--- src/controler/RemoveProductsServlet.java
+++ src/controler/RemoveProductsServlet.java
@@ -24,7 +24,7 @@ public class RemoveProductsServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String productId = request.getParameter("productId");
-		String valueToBeRemoved = request.getParameter("1");
+		String valueToBeRemoved = "1";
 		//String valueToBeRemoved = request.getParameter("productValue");
 		//zakodeno!
 		
